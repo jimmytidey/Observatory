b18bdb833770edd2ce72f7215ba58b0ac7dee1a4
Jimmy Tidey <jimmytidey@gmail.com>
2014-01-09 17:02:48 +0000

    saving tweets

--- server/parsers/twitter_user.js
+++ server/parsers/twitter_user.js
@@ -2,7 +2,7 @@ var util    = Meteor.require('util');
 var twitter = Meteor.require('twitter');
 
 Meteor.methods({
-    data_feed_go: function (url, parameter) {
+    data_feed_go: function (url, parameter, id) {
         if(url === 'twitter_user'){ 
   
             var twit = new twitter({
@@ -11,10 +11,22 @@ Meteor.methods({
                 access_token_key: global.observatory_keys['twitter_access_token'],
                 access_token_secret: global.observatory_keys['twitter_access_token_secret']
             });
-
-            twit.get('/statuses/user_timeline.json', {screen_name:'jimmytidey'}, function(data) {
-                console.log(data);
-                
+            
+            twit.listSync = Meteor._wrapAsync(twit.get.bind(twit));
+            
+            twit.listSync('/statuses/user_timeline.json', {screen_name:parameter}, function(data) {
+                for(var i=0; i< data.length; i++){ 
+                    var item = { 
+                        native_id: data[i].id_str, 
+                        title: data[i].text,
+                        author: parameter,
+                        author_id: data[i].user.id,
+                        content:'',
+                        feed_name: url
+                    } 
+                    console.log(item)
+                }
+                Items.insert(item);
             });
         }
     }
